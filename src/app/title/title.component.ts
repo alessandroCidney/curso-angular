d25b4dc0aa24417da62620bf37cbaad58cae06ac
alessandroCidney <contatoalessandrocidneysa@gmail.com>
2022-11-27 21:31:25 -0300

    feat: Added ngOnChanges method

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, Input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss']
 })
-export class TitleComponent implements OnInit {
-  public title: string = "Bem vindo!!!";
+export class TitleComponent implements OnInit, OnChanges {
+  @Input() public title: string = "Bem vindo!!!";
 
   constructor () { }
   
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('on changes')
   }
 }
