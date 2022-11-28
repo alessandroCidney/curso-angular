980d41877664287f211d0fb14e21cb6f264e5a61
alessandroCidney <contatoalessandrocidneysa@gmail.com>
2022-11-27 21:12:19 -0300

    feat: Added ngOnInit method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor () {
+    console.log('constructor')
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => console.log('on init'), 5000)
+  }
 }
