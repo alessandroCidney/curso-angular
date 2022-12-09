a03dae33f82585b5464b3b3f0a6d34c1231451ec
alessandroCidney <contatoalessandrocidneysa@gmail.com>
2022-12-08 22:42:32 -0300

    feat: Property binding, event binding and two-way binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
   styleUrls: ['./data-binding.component.scss']
 })
-export class DataBindingComponent {
-  public nome:string = "Pedro"
-  public idade:number = 20
+export class DataBindingComponent implements OnInit {
+  public nome: string = "Pedro"
+  public idade: number = 20
+  public checkedDisabled: boolean = true
+
+  public imgSrc: string = 'https://github.com/alessandroCidney.png'
+  public imgTitle: string = 'Property Binding'
+
+  public position: { x: number, y:number } = { x: 0, y: 0 }
+
+  constructor () {}
+
+  ngOnInit (): void {}
+
+  public alertaInfo (valor: MouseEvent): void {
+    console.log(valor)
+  }
+
+  public mouseMoveTest (event: MouseEvent): void {
+    // console.log(event)
+    this.position.x = event.offsetX
+    this.position.y = event.offsetY
+  }
 }
