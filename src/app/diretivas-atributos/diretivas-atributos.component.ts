96c93045b0282e4ee60639fba8a1011853ce5211
Alessandro Cídney <contatoalessandrocidneysa@gmail.com>
2023-08-16 00:10:41 -0300

    ngModel class

--- src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -7,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivasAtributosComponent implements OnInit {
   public valor: boolean = true
+  public altura: string = '20px'
+
+  public model1: string = ''
+
+  public model1List: Record<string, string>[] = []
+
+  public disableDivsCheckbox: boolean = false
 
   ngOnInit(): void {
     setInterval(() => {
       this.valor = !this.valor
+      this.altura = this.altura === '20px' ? '50px' : '20px'
     }, 2000)
+  }
+
+  saveModel1(): void {
+    this.model1List.push({ name: this.model1 })
+    this.model1 = ''
   }
 }
